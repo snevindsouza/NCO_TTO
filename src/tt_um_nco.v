59163ce7a64ba5c618dae9299bd005652f75504d
Snevin Leoneel Dsouza <81520536+snevindsouza@users.noreply.github.com>
2025-09-23 11:57:59 +0530

    Update tt_um_nco.v

--- src/tt_um_nco.v
+++ src/tt_um_nco.v
@@ -3,8 +3,8 @@ module tt_um_nco(
   input   wire  [2:0] signal_out,        // Select line
   output  reg   [7:0] wave_out           // Output to external DAC
 );
-reg [4:0] addr;				       // Address for the LUT (5-bit, 0-31)
-reg [7:0] wave_lut [0:31];	 // Lookup tables for different waveforms
+reg [4:0] addr;              // Address for the LUT (5-bit, 0-31)
+reg [7:0] wave_lut [0:31];   // Lookup tables for different waveforms
 integer i;
 always @(posedge clk_50MHz or posedge reset) begin
   if (reset) begin
